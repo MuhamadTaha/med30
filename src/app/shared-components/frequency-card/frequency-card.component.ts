7054d4e86af99f5fb7012831d282c6e3762ff60f
Muhamad Taha <melginady@youxel.com>
2023-02-12 04:57:55 +0200

    update-frequency-card

--- src/app/shared-components/frequency-card/frequency-card.component.ts
+++ src/app/shared-components/frequency-card/frequency-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-frequency-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./frequency-card.component.scss']
 })
 export class FrequencyCardComponent implements OnInit {
+
+  @Input() frequencyData: any;
 
   constructor() { }
 
