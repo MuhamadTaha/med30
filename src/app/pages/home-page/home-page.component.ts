f7ec92a9b89ac5c2e9313f8fc81046ceb2863562
Muhamad Taha <melginady@youxel.com>
2023-02-20 02:59:09 +0200

    update-home-page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -7,6 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  coverageTitle = 'Coverage rate'
+  interfactionalTitle = 'Interfactional view'
+  coverageChartData = {
+    labels: ['Covered', 'NotCovered'],
+    datasets: [
+      {
+        data: [4, 2],
+        backgroundColor: ["#3ba500", "#0870e7"]
+      }
+    ]
+  };
+  interfactionalChartData = {
+    labels: ['No. of interfactional views', 'No. of non-interfactional views'],
+    datasets: [
+      {
+        data: [2, 2],
+        backgroundColor: ["#3ba500", "#0870e7"]
+      }
+    ]
+  };
+
+  doctorsData = {
+    image: '../../../../assets/images/doctor-avatar.png',
+    name: 'Doctor name',
+    title: 'Title'
+  }
+
+  videosData = {
+    image: '../../../../assets/images/medicines.jpg',
+    name: 'Video name',
+    status: 'Published',
+    views: '3'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
